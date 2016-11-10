9a985d0ce0c9222ebed70afb5cbd05a8215417c1
Clarefollett <chlaw101@gmail.com>
2016-11-10 08:53:50 -0800

    updated my guessing game

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ var hobbiesAnswer = prompt('What do you like to do with your free time?');
 console.log('Fourth question answered with: ' + hobbiesAnswer);
 alert('Very interesting!');
 
+var count = 0;
+
 var firstYesOrNoQuestionAnswer = prompt('Do you like dogs?');
 
 console.log('User responded to the first question with: ' + firstYesOrNoQuestionAnswer);
 
 if (firstYesOrNoQuestionAnswer.toLowerCase() === 'yes' || firstYesOrNoQuestionAnswer.toLowerCase() === 'y') {
   alert('Good job. I love dogs!');
+  count = count++;
 } else if (firstYesOrNoQuestionAnswer.toLowerCase() === 'no' || firstYesOrNoQuestionAnswer.toLowerCase() === 'n') {
   alert('You have a dark soul');
 } else {
@@ -32,6 +35,7 @@ console.log('User responded to the second question with: ' + questionNumTwo);
 
 if (questionNumTwo.toLowerCase() === 'yes' || questionNumTwo.toLowerCase() === 'y') {
   alert('I have two very adorable schnoodles!');
+  count = count++;
 } else if (questionNumTwo.toLowerCase() === 'no' || questionNumTwo.toLowerCase() === 'n') {
   alert('Incorrect times two!');
 } else {
@@ -46,6 +50,7 @@ if (questionNumThree.toLowerCase() === 'yes' || questionNumThree.toLowerCase() =
   alert('Im not that crazy!');
 } else if (questionNumThree.toLowerCase() === 'no' || questionNumThree.toLowerCase() === 'n') {
   alert('Damn right! I think that would be terrifying and the Willamette isnt the safest water...');
+  count = count++;
 } else {
   alert('This is a yes or no question, dont think about it too hard!');
 }
@@ -56,6 +61,7 @@ console.log('User responded to the fourth question with: ' + questNumFour);
 
 if(questNumFour.toLowerCase() === 'yes' || questNumFour.toLowerCase() === 'y') {
   alert('I am what they call a Portland Native');
+  count = count++;
 } else if (questNumFour.toLowerCase() === 'no' || questNumFour.toLowerCase() === 'n') {
   alert('Actually, I was born and raised here! Pretty crazy stuff, huh?');
 } else {
@@ -68,8 +74,44 @@ console.log('User responded to the fifth question with: ' + lastQuest);
 
 if(lastQuest.toLowerCase() === 'yes' || lastQuest.toLowerCase() === 'y') {
   alert('I might but I am not at liberty to disclose such information.');
+  count = count++;
 } else if (lastQuest.toLowerCase() === 'no' || lastQuest.toLowerCase() === 'n') {
   alert('Maybe... Maybe not');
 } else {
   alert('I like your uniqueness!');
+}
+
+var questNumSix = prompt('How many minutes am I older than my twin?');
+
+console.log('User responded to the sixth question with: ' + questNumSix);
+
+for (var q = 0; q < 4; q++) {
+  console.log ('Run this four times');
+  if(questNumSix < 15) {
+    alert('Too low, Joe!');
+    questNumSix = prompt('Guess again!');
+  } else if(questNumSix > 15) {
+    alert('Too high like Wiz Khalifa');
+    questNumSix = prompt('Guess again!');
+  } else {
+    alert('Correctomundo!');
+    count = count++;
+    break;
+  }
+}
+
+var myArrayOfDogs = ['schnoodles','labs','muts','terriers', 'labradors', 'golden retrievers'];
+
+var questSeven = prompt('Name a breed of dog I have owned');
+
+for (var index = 0; index < myArrayOfDogs.length; index++) {
+  console.log('Run this six times');
+  if(questSeven === myArrayOfDogs[index]) {
+    alert('Correct!');
+    count = count++;
+    break;
+  } else {
+    alert('Incorrect!');
+    questSeven = prompt('Guess again!');
+  }
 }
